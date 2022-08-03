c84d0cf16e2f7c308241583dce3960c99d69334f
gamblor21 <mark.komus@gmail.com>
2022-08-03 17:27:50 -0500

    Removed accident double refresh

--- code.py
+++ code.py
@@ -97,12 +97,10 @@ while True:
     eye_left.update(eye_ax, eye_ay)
     eye_circle_left.x = int(eye_left.x) + SCREEN_RADIUS
     eye_circle_left.y = int(eye_left.y) + SCREEN_RADIUS
-    display_left.refresh()
 
     eye_right.update(eye_ax, eye_ay)
     eye_circle_right.x = int(eye_right.x) + SCREEN_RADIUS
     eye_circle_right.y = int(eye_right.y) + SCREEN_RADIUS
-    display_right.refresh()
 
     # experimental code to set a limit of FPS will skip frames if we are falling below this
     #if display.refresh(target_frames_per_second=120) is False:
